e6b0c713b31c17d1135393a5625f5c1af8ed92af
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2025-06-25 18:11:57 -0600

    Primer push
    Primer push antes de empezar

---
+++ EjercicioPractico1_JosefG/src/main/java/farmacia/com/EjercicioPracticoJosefGApplication.java
@@ -0,0 +1,13 @@
+package farmacia.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioPracticoJosefGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioPracticoJosefGApplication.class, args);
+	}
+
+}
---
+++ EjercicioPractico1_JosefG/src/test/java/farmacia/com/EjercicioPracticoJosefGApplicationTests.java
@@ -0,0 +1,13 @@
+package farmacia.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPracticoJosefGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
